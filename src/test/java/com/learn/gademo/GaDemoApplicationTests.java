21d10553278b0ee25cdfbf7a4d782d845f0f56a6
ja9q1x9 <Jafer_Ali_Jamal@gap.com>
2017-07-23 19:10:08 -0700

    prep app for pipeline

---
+++ src/test/java/com/learn/gademo/GaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.gademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GaDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
